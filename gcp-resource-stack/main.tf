e07928ce2a05b137907bd729a509784ffb30ef03
Rick te Brake <ricktebrake@gmail.com>
2021-12-27 17:02:56 +0100

    Add kubernetes resource

--- gcp-resource-stack/main.tf
+++ gcp-resource-stack/main.tf
@@ -1,5 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.3.0"
+    }
+  }
+}
+
 provider "google" {
-   project = "home-sensor-hub"
-   region = "europe-west1"
-   zone = "europe-west1-b"
-}  
+  credentials = file("home-sensor-hub-credentials.json")
+  project     = "home-sensor-hub"
+  region      = "europe-west1"
+  zone        = "europe-west1-b"
+}
+
+resource "google_container_cluster" "primary" {
+  name               = "home-sensor-hub-cluster"
+  location = "europe-west1"
+  enable_autopilot   = true
+  initial_node_count = 1
+}

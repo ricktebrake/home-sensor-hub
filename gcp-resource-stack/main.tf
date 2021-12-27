c79506a6e1d9f250e799ab552a969dc5d64e5878
Rick te Brake <ricktebrake@gmail.com>
2021-12-27 17:09:06 +0100

    Add variables to terraform stack.

--- gcp-resource-stack/main.tf
+++ gcp-resource-stack/main.tf
@@ -1,7 +1,20 @@
+variable "region" {
+  type        = string
+  description = "Google Cloud Region"
+  default     = "europe-west1"
+}
+
+variable "zone" {
+  type        = string
+  description = "Google Cloud Zone"
+  default     = "europe-west1-b"
+}
+
+
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "4.3.0"
     }
   }
@@ -10,13 +23,13 @@ terraform {
 provider "google" {
   credentials = file("home-sensor-hub-credentials.json")
   project     = "home-sensor-hub"
-  region      = "europe-west1"
-  zone        = "europe-west1-b"
+  region      = var.region
+  zone        = var.zone
 }
 
 resource "google_container_cluster" "primary" {
   name               = "home-sensor-hub-cluster"
-  location = "europe-west1"
+  location           = var.region
   enable_autopilot   = true
   initial_node_count = 1
 }

d6bc8e4d57a96ddc1a32bfd1810d26ffdeb32ac2
Rick te Brake <ricktebrake@gmail.com>
2022-01-03 12:51:29 +0100

    Print pubsubmessage attributes

--- process-sensor-telemetry/process-sensor-telemetry.go
+++ process-sensor-telemetry/process-sensor-telemetry.go
@@ -6,11 +6,39 @@ import (
 )
 
 type PubSubMessage struct {
-	Data []byte `json:"data"`
+	Data       []byte            `json:"data"`
+	Attributes map[string]string `json:"attributes"`
 }
 
 func ProcessTelemetry(ctx context.Context, m PubSubMessage) error {
 	log.Printf("%s", m.Data)
+	for key, value := range m.Attributes {
+		log.Println("Key:", key, "Value:", value)
+	}
+
+	//dbContext := context.Background()
+	//conf := &firebase.Config{ProjectID: "home-sensor-hub"}
+	//app, err := firebase.NewApp(ctx, conf)
+	//
+	//if (err != nil) {
+	//	log.Fatalln(err)
+	//}
+	//
+	//client, err := app.Firestore(dbContext)
+	//if (err != nil) {
+	//	log.Fatalln(err)
+	//}
+	//
+	//_, _, insertError := client.Collection("moisture-sensor").Add(dbContext, map[string]interface{}{
+	//	"timestamp": "",
+	//	"value":     "",
+	//})
+	//
+	//defer client.Close()
 
 	return nil
+}
+
+func handleError(err error) {
+
 }

8e2fb82e3511e245863f918cb5085769da5f6141
Rick te Brake <ricktebrake@gmail.com>
2022-01-06 17:51:23 +0100

    Just retrieve documents

--- plant-status-api/src/main/java/nl/ricktebrake/homesensorhub/PlantStatusResource.java
+++ plant-status-api/src/main/java/nl/ricktebrake/homesensorhub/PlantStatusResource.java
@@ -1,5 +1,6 @@
 package nl.ricktebrake.homesensorhub;
 
+import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
 import com.google.cloud.firestore.v1.FirestoreClient;
 import com.google.cloud.firestore.v1.FirestoreSettings;
@@ -12,6 +13,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.concurrent.ExecutionException;
 
 @Path("/hello")
 public class PlantStatusResource {
@@ -27,19 +29,33 @@ public class PlantStatusResource {
 
         log.info("Received request on /hello endpoint");
 
+
+
         return Multi.createFrom().emitter(emitter -> {
             emitter.emit(10000);
+
+//            sensorMeasurements.orderBy("timestamp", Query.Direction.DESCENDING).limit(1).addSnapshotListener((value, error) -> {
+//                if(error != null) {
+//                    log.error("Error adding listener to query", error);
+//                    emitter.complete();
+//                    throw error;
+//                }
+//                value.getDocuments().stream()
+//                        .map(document -> (Integer) document.getData().get("value"))
+//                        .forEach(emitter::emit);
+//            });
             CollectionReference sensorMeasurements = firestore.collection("moisture-sensor");
-            sensorMeasurements.orderBy("timestamp", Query.Direction.DESCENDING).limit(1).addSnapshotListener((value, error) -> {
-                if(error != null) {
-                    log.error("Error adding listener to query", error);
-                    emitter.complete();
-                    throw error;
-                }
-                value.getDocuments().stream()
-                        .map(document -> (Integer) document.getData().get("value"))
-                        .forEach(emitter::emit);
-            });
+            try {
+                QuerySnapshot query = sensorMeasurements.orderBy("timestamp", Query.Direction.DESCENDING).get().get();
+                query.getDocuments().forEach(document -> {
+                    emitter.emit((Integer) document.get("value"));
+                });
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+            emitter.complete();
         });
     }
 }

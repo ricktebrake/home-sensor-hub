c5074fbd1f3254fe616e34d5bffa5f5f122f1d2c
Rick te Brake <ricktebrake@gmail.com>
2022-01-06 14:06:55 +0100

    Add logging and emit initial value

--- plant-status-api/src/main/java/nl/ricktebrake/homesensorhub/PlantStatusResource.java
+++ plant-status-api/src/main/java/nl/ricktebrake/homesensorhub/PlantStatusResource.java
@@ -6,7 +6,9 @@ import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.Query;
 import com.google.cloud.firestore.v1.FirestoreClient;
 import com.google.cloud.firestore.v1.FirestoreSettings;
+import io.quarkus.logging.Log;
 import io.smallrye.mutiny.Multi;
+import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -20,11 +22,16 @@ public class PlantStatusResource {
     @Inject
     private Firestore firestore;
 
+    private static final Logger log = Logger.getLogger(PlantStatusResource.class);
+
     @GET
     @Produces(MediaType.SERVER_SENT_EVENTS)
     public Multi<Integer> hello() {
 
+        log.info("Received request on /hello endpoint");
+        
         return Multi.createFrom().emitter(emitter -> {
+            emitter.emit(10000);
             CollectionReference sensorMeasurements = firestore.collection("moisture-sensor");
             sensorMeasurements.orderBy("timestamp", Query.Direction.DESCENDING).limit(1).addSnapshotListener((value, error) -> {
                 emitter.emit(value.getDocuments()

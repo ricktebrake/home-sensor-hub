d7e9b5f5a0a20844937edc5a8aba44eec236d3aa
Rick te Brake <ricktebrake@gmail.com>
2022-01-07 19:30:16 +0100

    - Add API to retrieve values in a data range. - Add memory limits - Add lombok dependency - Add local-dev config

--- plant-status-api/src/main/java/nl/ricktebrake/homesensorhub/PlantStatusResource.java
+++ plant-status-api/src/main/java/nl/ricktebrake/homesensorhub/PlantStatusResource.java
@@ -1,20 +1,20 @@
 package nl.ricktebrake.homesensorhub;
 
-import com.google.cloud.firestore.CollectionReference;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.Query;
-import com.google.cloud.firestore.QuerySnapshot;
+import com.google.cloud.firestore.*;
 import io.smallrye.mutiny.Multi;
+import lombok.Data;
 import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.util.concurrent.ExecutionException;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.Date;
 
-@Path("/hello")
+@Path("/plantstatus")
 public class PlantStatusResource {
 
     @Inject
@@ -23,16 +23,20 @@ public class PlantStatusResource {
     private static final Logger log = Logger.getLogger(PlantStatusResource.class);
 
     @GET
+    @Path("/{deviceId}")
     @Produces(MediaType.SERVER_SENT_EVENTS)
-    public Multi<Long> hello() {
+    public Multi<Long> getCurrentValueForDevice(@PathParam("deviceId") String deviceId) {
 
         log.info("Received request on /hello endpoint");
 
-
         return Multi.createFrom().emitter(emitter -> {
-            emitter.emit(10000L);
             CollectionReference sensorMeasurements = firestore.collection("moisture-sensor");
-            sensorMeasurements.orderBy("timestamp", Query.Direction.DESCENDING).addSnapshotListener((value, error) -> {;
+            ListenerRegistration listenerRegistration = sensorMeasurements
+                    .whereEqualTo("deviceId", deviceId)
+                    .orderBy("timestamp", Query.Direction.DESCENDING)
+                    .limit(1)
+                    .addSnapshotListener((value, error) -> {
+
                 if (error != null) {
                     log.error("Error adding listener to query", error);
                     emitter.complete();
@@ -44,6 +48,44 @@ public class PlantStatusResource {
                         .map(document -> document.getLong("value"))
                         .ifPresent(emitter::emit);
             });
+            emitter.onTermination(listenerRegistration::remove);
         });
     }
+
+    @GET
+    @Path("/{deviceId}/historical")
+    @Produces(MediaType.SERVER_SENT_EVENTS)
+    public Multi<MoistureValue> getValuesForTimeRange(@PathParam("deviceId") String deviceId, @QueryParam("start") String start, @QueryParam("end") String end) {
+        var startDate = LocalDate.parse(start).atStartOfDay();
+        var endDate = LocalDate.parse(end).atStartOfDay().plusDays(1);
+
+        return Multi.createFrom().emitter(emitter -> {
+            CollectionReference sensorMeasurements = firestore.collection("moisture-sensor");
+            ListenerRegistration listenerRegistration = sensorMeasurements
+                    .whereEqualTo("deviceId", deviceId)
+                    .whereGreaterThan("timestamp", Date.from(startDate.toInstant(ZoneOffset.UTC)))
+                    .whereLessThan("timestamp", Date.from(endDate.toInstant(ZoneOffset.UTC)))
+                    .orderBy("timestamp", Query.Direction.DESCENDING)
+                    .addSnapshotListener((value, error) -> {
+
+                        if (error != null) {
+                            log.error("Error adding listener to query", error);
+                            emitter.complete();
+                            throw error;
+                        }
+                        log.info(String.format("New database event"));
+                        value.getDocumentChanges().stream()
+                                .map(document -> new MoistureValue(document.getDocument().getLong("value"),
+                                        ZonedDateTime.ofInstant(document.getDocument().getDate("timestamp").toInstant(), ZoneId.of("CET"))))
+                                .forEach(emitter::emit);
+                    });
+            emitter.onTermination(listenerRegistration::remove);
+        });
+    }
+}
+
+@Data
+class MoistureValue {
+    private final Long value;
+    private final ZonedDateTime timestamp;
 }
